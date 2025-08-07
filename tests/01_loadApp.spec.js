93cfc96038f82cb6b5e8b7d9a0d0a76a041802c6
Ayobami Elutade <aelutade@1wa.org>
2025-08-07 19:28:59 +0100

    add a backslach to base url

--- tests/01_loadApp.spec.js
+++ tests/01_loadApp.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test')
 const BASE_URL = process.env.BASE_URL
 
 test('Loading the admin dashboard app', async ({ page }) => {
-  await page.goto(`${BASE_URL}`)
+  await page.goto(`${BASE_URL}/`)
 
-  await expect(page).toHaveURL(`${BASE_URL}`)
+  await expect(page).toHaveURL(`${BASE_URL}/`)
 })
